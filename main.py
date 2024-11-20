452ab6b276822b543946ca3a2a27caabbc69f3fe
baludevy <babjakbalazs5@gmail.com>
2024-11-20 20:18:01 +0100

    i think its done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from prisma import Prisma
 from contextlib import asynccontextmanager
 
@@ -17,31 +18,48 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.post("/add_host")
-async def add_host(ip_addr: str, mac_addr: str):
+async def add_host(name: str, ip_addr: str, mac_addr: str):
     if validate_ip(ip_addr) and validate_mac(mac_addr):
-        data = await db.host.create({"ip_address": ip_addr, "mac_address": mac_addr})
+        data = await db.host.create(
+            {"name": name, "ip_address": ip_addr, "mac_address": mac_addr}
+        )
         return data
     return JSONResponse({"message": "invalid input"}, status_code=400)
+
 
 @app.post("/delete_host")
 async def delete_host(host: str):
     try:
-        await db.host.delete(where={'id': host})
+        await db.host.delete(where={"id": host})
         return JSONResponse({"message": "cool"})
-    except:    
+    except:
         return JSONResponse({"message": "host not found"}, status_code=404)
 
 
 @app.post("/send_wol")
 async def send_wol(host: str):
     try:
-        data = await db.host.find_unique_or_raise(where={'id': host})
+        data = await db.host.find_unique_or_raise(where={"id": host})
         wol(data.mac_address)
         return JSONResponse({"message": "cool"})
     except:
         return JSONResponse({"message": "host not found"}, status_code=404)
-    
+
 
 @app.get("/list_hosts")
 async def list_hosts():
@@ -49,9 +67,9 @@ async def list_hosts():
     return data
 
 
-
 def wol(mac_addr: str) -> None:
     print(f"yeah i absolutely sent the wol request to {mac_addr}")
+
 
 def validate_ip(ip: str) -> bool:
     regex = r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
